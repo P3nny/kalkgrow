f70cc56985eb702ae22454eda79b3d53ad1b366e
P3nny <p3nny@hacking.studio>
2020-02-07 16:14:47 +0100

    🐛 Remove test button

--- camera.sh
+++ camera.sh
@@ -1,9 +1,9 @@
-#! bin/bash
+#!/bin/bash
 
 while [ : ]
 do
     TIMESTAMP=$(date +%F-%H-%M-%S)
-    ffmpeg -f video4linux2 -i /dev/video2 -vframes 1 $TIMESTAMP.jpg
-    s3cmd put --acl-public $TIMESTAMP.jpg s3://kalkgrow/plant.jpg
+    ffmpeg -f video4linux2 -i /dev/video2 -vframes 1 ./img/$TIMESTAMP.jpg
+    s3cmd put --acl-public ./img/$TIMESTAMP.jpg s3://kalkgrow/plant.jpg
     sleep 12
 done
